b4d7df47a564b826a9396b8ba85d73284b7280f1
umangsain <umangsain66@gmail.com>
2026-09-15 15:11:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Step 1: Load Important Modules
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -7,25 +8,40 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score
 import streamlit as st
-# this stremlit is for web based application project
+# this streamlit is for web based application project
+
 
 # Web Page Code
 st.title("HEALTH INSURANCE PREDICTION")
-img_url = "https://www.google.com/imgres?q=health%20insurance&imgurl=https%3A%2F%2Fwww.fundsindia.com%2Fblog%2Fwp-content%2Fuploads%2F2014%2F07%2Fhealth_insurance.jpg&imgrefurl=https%3A%2F%2Ffundsindia.com%2Fblog%2Fpersonal-finance%2Fseven-secrets-to-choose-the-best-health-insurance-plan%2F5649&docid=eMxRwHovuGfqRM&tbnid=UXgBZd1nRWTJHM&vet=12ahUKEwicrcWdgvCWAxUuRmwGHU3RGjsQnPAOegUI3wEQAA..i&w=550&h=465&hcb=2&ved=2ahUKEwicrcWdgvCWAxUuRmwGHU3RGjsQnPAOegUI3wEQAA"
-st.img(img_url)
+img_url = "https://cdn.zeebiz.com/sites/default/files/2026/03/09/401943-health-insurance.png"
+st.image(img_url)
 
-# Load Data and ML MODEL PART 
+# LOAD DATA and ML MODEL PART
+
+# Step 2: load Insurance data
 url = "https://raw.githubusercontent.com/ankitmisk/UIT-data/refs/heads/main/Insurance.csv"
 df = pd.read_csv(url)
-df.drop("Customer_ID", axis = 1, inplace=True)
-df['Previous_Insurance'] = df['Previous_Insurance'].map({'Yes':1, 'No':0})
-df['Insurance_Bought'] = df['Insurance_Bought'].map({'Yes':1, 'No':0})
+
+# Step 3: EDA: Exploratory Data Analysis
+df.drop("Customer_ID", axis = 1, inplace = True)
+
+df['Previous_Insurance'] = df['Previous_Insurance'].map({'No':0,"Yes":1})
+df['Insurance_Bought'] = df['Insurance_Bought'].map({'No':0,"Yes":1})
+
+
+# Step 4: Divide dataset into features and target
 X = df.iloc[:,:-1]
 y = df.iloc[:,-1]
+
+# Step 5: Divide data into Training & testing part
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
+X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, test_size=0.3)
+
+# Step  6: Train Model
 model = LogisticRegression()
-model.fit(X_train, y_train)
+model.fit(X_train,y_train)
+
+
 
 # show data sample
 st.write(df.head())
@@ -33,32 +49,36 @@ st.write(df.head())
 st.sidebar.title("Fill Customer Details")
 st.sidebar.image(img_url)
 
+
+# To get user input
+all_ans = []
 for index, col_name in enumerate(X.columns):
   min_v = X[col_name].min()
   max_v = X[col_name].max()
   if col_name != "Previous_Insurance":
     value = st.sidebar.slider(f"Select value for {col_name}",
-                              min_value = min_y,
-                              max_value = max_y)
+                             min_value = min_v,
+                             max_value = max_v)
   else:
     value = st.sidebar.number_input(f"Select value for {col_name} (0:No, 1: yes): ")
 
-all_ans.append(value)
+  all_ans.append(value)
 
 ud = {j:all_ans[i] for i,j in enumerate(X.columns)}
 user_df = pd.DataFrame(ud, index = [1])
 st.write(user_df)
 
-#============================Prediction================
-
+#=========================Prediction==============
 if st.button("Click to Predict: "):
-    with st.spinner("Predicting.."):
-        import time
-        time.sleep(2)
+  with st.spinner("Predicting.."):
+    import time
+    time.sleep(2)
+  final_ans = model.predict([all_ans])[0]
+  if final_ans == 0:
+    st.info("❎Customer will not Buy the Insurance❎")
+  else:
+    st.success("✅Customer will buy the Insurance✅")
 
-    final_ans = model.predict([all_ans])[0]
 
-    if final_ans == 0:
-        st.info("❌Customer will not Buy the Insurance❌")
-    else:
-        st.success("✅Customer will buy the Insurance✅") 
+# changes 1: dict
+# changes 2: model predict
